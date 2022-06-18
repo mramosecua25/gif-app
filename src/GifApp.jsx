bb8538b2743eadb911d518a53752a31069fb24e0
Marcelo Ramos <mramosecua@gmail.com>
2022-06-18 17:27:43 -0400

    add category

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
 
 export const GifApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+
+  const onAddCategory = () => {
+    setCategories(['Spiderman', ...categories])
+  }
+
   return (
     <>
-        <div>GifApp</div>
+      <h1>Gif App</h1>
+
+      <button onClick={ onAddCategory }>Agregar</button>
+      <ol>
+          {
+            categories.map(category => {
+              return <li key={ category }>{ category }</li>
+            })
+          }
+        </ol>
     </>
   )
 }
